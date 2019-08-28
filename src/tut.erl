b829c03b1802578c4f4c283b1abc17544c495faa
Emil Ståhl <emil.stahl@icloud.com>
2019-08-28 19:51:40 +0200

    sum

--- src/tut.erl
+++ src/tut.erl
@@ -9,9 +9,7 @@
 -export([main/0]).
 
 main() -> 
-	find_factorial(3).
+	sum([1,2,3,4,5]).
 
-factorial(N) when N == 0 -> 1;
-factorial(N) when N > 0 -> N * factorial(N-1).
-							
-find_factorial(N) -> factorial(N).
+sum([]) -> 0;
+sum([H|T]) -> H + sum(T).
