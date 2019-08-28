6e730e1f56a295fba2646cf0cf5cbd3ae9c2e430
Emil Ståhl <emil.stahl@icloud.com>
2019-08-28 17:19:20 +0200

    string stuff

--- src/tut.erl
+++ src/tut.erl
@@ -4,16 +4,31 @@
 
 -module(tut).
 
--import(string, [len/1, concat/2, chr/2, sustr/3, str/2, to_lower/1, to_upper/1]).
+-import(string, [len/1, concat/2, chr/2, substr/3, str/2, to_lower/1, to_upper/1]).
 
 -export([main/0]).
 
 main() ->
-	say_hello(english).
+	string_stuff().
 
-say_hello(X) ->
-	case X of 
-		german -> 'guten tag';
-		french -> "bonjour";
-		english -> "hello"
-end.
+string_stuff() -> 
+	Str1 = "Random string",
+	Str2 = "Another string",
+	
+	io:fwrite("String : ~p ~p\n", [Str1, Str2] ),
+
+	Str3 = io_lib:format("It's a ~s and ~s\n", [Str1, Str2]),
+	io:fwrite(Str3),
+	
+	len(Str3),
+
+	Str4 = concat(Str1, Str2),
+	Str4,
+
+	Str5 = substr(Str4, 14,7),
+	Str5,
+	
+	StrIndex = str(Str4, Str2),
+	StrIndex,
+	
+	to_upper(Str1).
