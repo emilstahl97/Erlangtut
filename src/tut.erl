a79cc12a5c3c47192263b431c59827d6487340a1
Emil Ståhl <emil.stahl@icloud.com>
2019-08-30 14:27:17 +0200

    Added some processes to test

--- src/tut.erl
+++ src/tut.erl
@@ -9,9 +9,13 @@
 -export([main/0]).
 
 main() -> 
-	macro_stuff(5,6).
 
--define(add(X,Y), {X+Y}).
+	spawner(),
+	spawner(),
+	spawner().
+	
+get_id(M) ->
+	io:fwrite("ID : ~p\n", [M]).
 
-macro_stuff(X,Y) ->
-	io:fwrite("~p\n", [?add(X,Y)]).
+spawner() ->
+	spawn(fun() -> get_id([self()]) end).
